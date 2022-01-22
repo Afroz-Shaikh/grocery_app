a7f7ff704acfe23ae91acbe344394880c12c31ef
Afroz-Shaikh <afrozshaikh725@gmail.com>
2022-01-22 16:37:13 +0530

    Details added

--- lib/views/product_detail.dart
+++ lib/views/product_detail.dart
@@ -9,8 +9,10 @@ class ProductDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     final productId =ModalRoute.of(context)?.settings.arguments as String;
 final loadedProduct=Provider.of<Products>(context).items.firstWhere((prod)=>prod.id==productId);
+// final productsi = Prodvider.of<Products>(context).items
 
     return Scaffold(
       appBar: AppBar(
@@ -21,7 +23,87 @@ final loadedProduct=Provider.of<Products>(context).items.firstWhere((prod)=>prod
 // title: productId,
 title: Text(loadedProduct.name,style: TextStyle(color: Colors.black),),
       ),
-      
+      body: Column(
+children: [
+  ClipRRect(
+    borderRadius: BorderRadius.circular(15),
+    child: Container(
+      child: Image.asset(loadedProduct.imageUrl)
+      // Image.image: Image.asset(loadedProduct.imageUrl),
+      ),
+    ),
+  
+  ClipRRect(
+    child: Expanded(
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            
+            Center(
+              child: Text(loadedProduct.name),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Price')
+                  ,
+                  Text(loadedProduct.cost.toString()),
+                ],
+              ),
+              
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  
+                  Text(loadedProduct.desc),
+                  Text(loadedProduct.category),
+                  
+                ],
+              ),
+              
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  
+                  Text('Availablility'),
+                  loadedProduct.available ? Icon(Icons.check) : Icon(Icons.error)
+                  
+                ],
+              ),
+              
+            ),
+// Spacer(),
+            Card(
+              color: Colors.green,
+              child: Container(child: Center(child: Text('Buy now',style: TextStyle(color: Colors.white,fontSize: 50),)),
+              height: 50,
+              width: double.infinity,),
+              )
+
+            
+            // Text(loadedProduct.)
+          ],
+        ),
+    
+      ),
+    ),
+  )
+],
+
+
+
+      ),
     );
   }
 }
